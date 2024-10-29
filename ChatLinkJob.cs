8846a2e57f83c58c161b0aac701f3aa3aad7d1bf
U_M26M1 <igusev2@alfabank.ru>
2024-10-29 14:22:30 +0300

    Initial commit

---
+++ ChatLinkJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace TreechChatLinkCreator;
+
+public class HelloWorld : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine("wowowowowowoowowowo");
+        return Task.CompletedTask;
+    }
+}
